834e72c972f008c5007cc8e2c656adf6147871bd
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-04-24 16:49:49 +0800

    feat: 🎸 功能完善: 帮助文档
    使用 package.json 信息来展示 cli 的版本号等信息

--- src/cli.js
+++ src/cli.js
@@ -2,16 +2,24 @@
 
 const program = require('caporal');
 const creatDirectory = require('./actions/create-directory');
+const path = require('path');
+const { readFileOrEmpty, parseOrFalse } = require('./util');
+
+const pkgFile = path.join(__dirname, '../package.json');
+const pkgJson = parseOrFalse(readFileOrEmpty(pkgFile));
 
 // ref: https://www.sitepoint.com/scaffolding-tool-caporal-js/
 // bar.tick();};
 
 program
-  .version('0.1.2')
-  .description('ide 命令行工具集')
+  .version(pkgJson.version || 'unknown')
+  .description(pkgJson.description || 'ide 命令行工具')
   .command('create', 'create - 创建新的 ide 模块')
   .argument('<jsonfile>', 'config json file - json 格式的配置文件')
-  .option('-l, --local', 'not git clone repository - 不从远程拉取仓库（方便本地调试）')
+  .option(
+    '-l, --local',
+    'not git clone repository - 不从远程拉取仓库（方便本地调试）'
+  )
   .action(creatDirectory);
 
 program.parse(process.argv);
